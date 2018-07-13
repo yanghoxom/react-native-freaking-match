0e1b014fbeeed3946eaedd93041147fe053df779
memsenpai <memsenpai3@gmail.com>
2018-07-13 13:30:21 +0700

    build

--- App.js
+++ App.js
@@ -1,36 +1,104 @@
 import React, { Component } from 'react';
-import { View, Text, TouchableHighlight, Alert } from 'react-native';
+import { View, Text, TouchableHighlight, Alert, Dimensions, Modal, BackHandler } from 'react-native';
+import FlashScreen from './app/components/FlashScreen'
 
 export default class FreakingMath extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      number_one: 1,
-      number_two: 2
+      score: -1,
+      math: '',
+      result: '',
+      result_show: '',
+      loop: '',
+      timeCounter: Dimensions.get('window').width,
+      width_screen: Dimensions.get('window').width,
+      modalVisible: true,
+      end: false
+    }
+    this.initMath = this.initMath.bind(this);
+  }
+
+  initMath() {
+    clearInterval(this.state.loop);
+    const list_oparetors_easy = ['+', '-'];
+    const one = Math.floor(Math.random() * 10);
+    const two = Math.floor(Math.random() * 10);
+    const operator = list_oparetors_easy[Math.floor(Math.random() * list_oparetors_easy.length)];
+    const result = eval('one ' + operator + ' two');
+    const result_show = eval('result '+ list_oparetors_easy[Math.floor(Math.random() * list_oparetors_easy.length)] + ' Math.floor(Math.random() * 2)');
+    this.setState({
+      math: '' + one + operator + two + '=' + result_show,
+      result: result,
+      result_show: result_show,
+      score: this.state.score += 1
+    });
+    this.countDown();
+  }
+
+  countDown() {
+    const loop = setInterval(() => {
+      if(this.refs.timeCounter && !this.state.end){
+        this.refs.timeCounter.measure((ox, oy, width, height) => {
+          this.setState({timeCounter: width - this.state.width_screen/20});
+        });
+        if(this.state.timeCounter <= this.state.width_screen/20){
+          clearInterval(loop); 
+          this.setState({end: true});
+        };
+      }
+    }, 50);
+    this.setState({loop: loop});
+  }
+
+  onPressTouchableHighlight(boolean) {
+    clearInterval(this.state.loop);
+   if((this.state.result == this.state.result_show) == boolean){
+    this.setState({timeCounter: Dimensions.get('window').width});
+    this.initMath();
+   } else {
+    this.setState({end: true});
+   } 
+  }
+
+  onTimeover() {
+    if(this.state.end){
+      Alert.alert('Opps',
+        'Gem over, score: ' + this.state.score,
+        [
+          {
+            text: 'Again', onPress: () => {
+              this.setState({score: -1, end: false});
+              this.initMath();
+            }
+          }
+        ]
+      );
     }
   }
 
-  onPressTouchableHighlight() {
-    Alert.alert('You taaped the TouchableHighlight!');
-  }
-
-  onPressTouchableHighlightTwo() {
-    Alert.alert('You taaped the TouchableHighlight2!');
-  }
-
   render() {
+    this.onTimeover();
     return (
       <View style={{flex: 1}}>
-        <View style={{flex: 3, backgroundColor: 'powderblue'}}></View>
+        <FlashScreen initMath={this.initMath}/>
+        <View style={{flex: 3, backgroundColor: 'powderblue'}}>
+          <View ref="timeCounter" style={{width: this.state.timeCounter,height: 35, marginTop: 50, backgroundColor: 'green'}}>
+            <Text style={{fontSize:30, color: 'white'}}>{this.state.score}</Text>
+          </View>
+          <View style={{height: 100, marginVertical: 180, marginHorizontal: 10}}>
+            <Text style={{fontSize:50, color: 'white', textAlign: 'center'}} ref="math">{this.state.math}</Text>
+          </View>
+        </View>
         <View style={{flex: 1, backgroundColor: 'skyblue', flexDirection: 'row', justifyContent: 'space-around', alignItems: 'center'}}>
           <TouchableHighlight
             style={{flexGrow: 1, alignSelf: 'stretch', justifyContent: 'center', alignItems: 'center'}}
-            onPress={this.onPressTouchableHighlight}>
+            onPress={() => this.onPressTouchableHighlight(true)}>
             <Text style={{fontSize:24, color: 'white'}}>True</Text>
           </TouchableHighlight>
           <TouchableHighlight
             style={{flexGrow: 1, alignSelf: 'stretch', justifyContent: 'center', alignItems: 'center'}}
-            onPress={this.onPressTouchableHighlightTwo}>
+            onPress={() => this.onPressTouchableHighlight(false)}>
             <Text style={{fontSize:24, color: 'white'}}>False</Text>
           </TouchableHighlight>
         </View>
---
+++ app/components/FlashScreen.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import { View, Text, TouchableHighlight, Alert, Dimensions, Modal, BackHandler } from 'react-native';
+
+export default class FlashScreen extends Component {
+  state = {
+    modalVisible: true
+  }
+
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', () => {
+      !this.state.modalVisible ? this.setModalVisible(true) : '';
+    });
+  }
+
+  setModalVisible(visible) {
+    this.setState({modalVisible: visible});
+    !visible ? this.props.initMath() : '';
+  }
+  
+  render() {
+    return (
+      <Modal
+        animationType="slide"
+        transparent={false}
+        visible={this.state.modalVisible}
+        style={{flex: 1}}
+        onRequestClose={() => {
+          ''
+        }}>
+        <View style={{flex: 1, marginTop: 200, marginLeft: 50, marginRight: 50, marginBottom: 200}}>
+          <View style={{flex: 1}}>
+            <TouchableHighlight
+              onPress={() => {
+                this.setModalVisible(!this.state.modalVisible);
+              }}
+              style={{flexGrow: 1, alignSelf: 'stretch', justifyContent: 'center', alignItems: 'center', backgroundColor: 'green'}}>
+              <Text style={{fontSize: 50}}>Play</Text>
+            </TouchableHighlight>
+          </View>
+        </View>
+      </Modal>
+    )
+  }
+}
